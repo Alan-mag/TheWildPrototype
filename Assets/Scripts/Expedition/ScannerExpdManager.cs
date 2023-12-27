e78b1cb0d1eaa6a137d0aac906b851f12023d5ba
alan <alan@geocaching.com>
2023-12-26 21:07:56 -0800

    Update audio for expedition - fmod

--- Assets/Scripts/Expedition/ScannerExpdManager.cs
+++ Assets/Scripts/Expedition/ScannerExpdManager.cs
@@ -21,6 +21,9 @@ public class ScannerExpdManager : MonoBehaviour
     SceneChangeHandler sceneChangeHandler;
     [SerializeField] ExpeditionLevelHandler expeditionSceneHandler;
 
+    [SerializeField]
+    HandleFmodEvent handleFmodEvent;
+
     private void Awake()
     {
         if (sceneChangeHandler == null)
@@ -90,8 +93,9 @@ public class ScannerExpdManager : MonoBehaviour
 
     private void PlayScanningAudio()
     {
-        AmazonPollyUtil pollyUtil = this.GetComponent<AmazonPollyUtil>();
-        pollyUtil.PlayMessageWithPolly("Japanese maple has long been cultivated in Japan and was introduced into cultivation in Europe in the early 1800s. It is one of the most versatile small trees for use in the landscape. It exists in a multitude of forms that provide a wide range of sizes, shapes, and colors.");
+        // AmazonPollyUtil pollyUtil = this.GetComponent<AmazonPollyUtil>();
+        // pollyUtil.PlayMessageWithPolly("Japanese maple has long been cultivated in Japan and was introduced into cultivation in Europe in the early 1800s. It is one of the most versatile small trees for use in the landscape. It exists in a multitude of forms that provide a wide range of sizes, shapes, and colors.");
+        handleFmodEvent.PlayFmodEvent();
     }
 
     private void OnDestroy()
